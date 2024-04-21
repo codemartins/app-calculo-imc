68444d882ce03c051df17a5b1a6aba98b86c9234
Germeson Martins <codemartins@outlook.com>
2024-04-21 15:47:46 -0300

    Formula de calculo

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App(){
+  const [peso, setPeso] = useState('');
+  const [altura, setAltura] = useState ('');
+
+  function handleSumit(){
+    const alt = altura / 100;
+    const imc = peso / (alt * alt);
+    
+    if (imc < 18.6){
+      alert('Você está abaixo do Peso! ' + imc.toFixed(2));
+    }else if (imc >= 18.6 && imc < 24.9){
+      alert('Peso ideal! ' + imc.toFixed(2));
+    }else if (imc >= 24.9 && imc < 34.9){
+      alert('Levemente acima do Peso! ' + imc.toFixed(2));
+    }
+  }
+
   return(
     <View style={styles.container}>
-      <Text style={styles.titulo}>Meu App</Text>
-      <Text style={styles.descricao}>Workshop do zero ao app</Text>
+      <Text style={styles.title}>Calcule seu IMC</Text>
+
+      <TextInput
+      style={styles.input}
+      value={peso}
+      onChangeText={ (peso) => setPeso(peso) }
+      placeholder="Peso (Kg)"
+      keyboardType="numeric"
+      />
+
+      <TextInput
+      style={styles.input}
+      value={altura}
+      onChangeText={ (altura) => setAltura(altura)}
+      placeholder="Altura"
+      keyboardType="numeric"
+      />
+
+      <TouchableOpacity 
+      style={styles.button}
+      onPress={handleSumit}
+      >
+        <Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,18 +52,34 @@ export default function App(){
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    backgroundColor: '#FF0000',
+   
+  },
+
+  title:{
+    textAlign: 'center',
+    marginTop: 25,
+    fontSize: 30,
+  },
+
+  input:{
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    color: '#FFF',
+    fontSize: 23,
+  },
+  button:{
     justifyContent: 'center',
     alignItems: 'center',
+    margin: 15,
+    backgroundColor: '#41Aef4',
+    padding: 10,
   },
-  titulo:{
-    fontSize: 30,
-    color: '#FFF'
-  },
-  descricao:{
-    fontSize: 20,
-    color: '#00FF00'
 
+  buttonText:{
+    color: '#FFF',
+    fontSize: 25,
   }
 
 });
